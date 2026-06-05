6502c0c4fec692bb0e6f97063efeb92c94a205e8
umhye1 <lucy021206@gmail.com>
2026-06-05 14:04:08 +0900

    feat(auth): jwt 구현

--- src/main/java/com/finguard/auth/service/AuthService.java
+++ src/main/java/com/finguard/auth/service/AuthService.java
@@ -4,6 +4,7 @@ import com.finguard.auth.dto.request.LoginRequest;
 import com.finguard.auth.dto.request.LogoutRequest;
 import com.finguard.auth.dto.response.LoginResponse;
 import com.finguard.auth.dto.response.MyInfoResponse;
+import com.finguard.auth.jwt.JwtTokenProvider;
 import com.finguard.global.exception.DuplicateEmailException;
 import com.finguard.global.exception.LoginFailedException;
 import com.finguard.global.exception.UnauthorizedException;
@@ -24,6 +25,7 @@ public class AuthService {
 
     private final UserRepository userRepository; // DB 접근 객체를 AuthService에서 사용하려고 주입받는 것
     private final PasswordEncoder passwordEncoder; // 비밀번호 암호화 위해 사용 - 나중에 로그인할 때는 입력한 비밀번호와 암호화된 비밀번호를 비교
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional // 회원가입은 조회만 하면 안됨 - transactional 다시 붙임
     public SignupResponse signup(SignupRequest request) {
@@ -51,8 +53,16 @@ public class AuthService {
             throw new LoginFailedException("이메일 또는 비밀번호가 올바르지 않습니다.");
         }
 
-        String accessToken = "temporary-access-token"; // 임시 토큰
-        String refreshToken = "temporary-refresh-token";
+        String accessToken = jwtTokenProvider.createAccessToken(
+                user.getUserId(),
+                user.getEmail(),
+                user.getRole()
+        );
+
+        String refreshToken = jwtTokenProvider.createRefreshToken(
+                user.getUserId(),
+                user.getEmail()
+        );
 
         return LoginResponse.of(user, accessToken, refreshToken);
     }
@@ -60,13 +70,17 @@ public class AuthService {
     @Transactional
     public void logout(String authorizationHeader, LogoutRequest request) {
         String accessToken = extractAccessToken(authorizationHeader);
-        System.out.println("logout accessToken = " + accessToken);
-        System.out.println("logout refreshToken = " + request.getRefreshToken());
+        if (!jwtTokenProvider.validateToken(accessToken)) {
+            throw new UnauthorizedException("로그아웃에 실패했습니다.");
+        }
 
+        if (request.getRefreshToken() == null || !jwtTokenProvider.validateToken(request.getRefreshToken())) {
+            throw new UnauthorizedException("로그아웃에 실패했습니다.");
+        }
     }
 
     private String extractAccessToken(String authorizationHeader) {
-        if (authorizationHeader == null && !authorizationHeader.startsWith("Bearer ")) {
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             throw new UnauthorizedException("인증 정보가 올바르지 않습니다.");
         }
         return authorizationHeader.substring(7);
@@ -75,10 +89,16 @@ public class AuthService {
     @Transactional(readOnly = true)
     public MyInfoResponse getMyInfo(String authorizationHeader) {
         String accessToken = extractAccessToken(authorizationHeader);
-        // TODO: JWT에서 email 추출해야 함
-        // String email = jwtTokenProvider.getEmail(accessToken);
 
-        // 임시 테스트용으로는 토큰만으로 이메일을 알 수 없어서 실제 구현 불가
-        throw new UnauthorizedException("내 정보 조회에 실패했습니다.");
+        if(!jwtTokenProvider.validateToken(accessToken)) {
+            throw new UnauthorizedException("내 정보 조회에 실패했습니다.");
+        }
+
+        String email = jwtTokenProvider.getEmail(accessToken);
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new UnauthorizedException("내 정보 조회에 실패했습니다."));
+
+        return MyInfoResponse.from(user);
     }
 }
--- src/main/java/com/finguard/global/config/SecurityConfig.java
+++ src/main/java/com/finguard/global/config/SecurityConfig.java
@@ -1,30 +1,47 @@
 package com.finguard.global.config;
 
+import com.finguard.auth.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf->csrf.disable())
-                .formLogin(form->form.disable())
-                .httpBasic(httpBasic->httpBasic.disable())
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-//                                로그인 없이 접근 가능하게 함, 나머지는 인증 필요하게 둠
                                 "/actuator/health",
                                 "/api/auth/signup",
                                 "/api/auth/login"
                         ).permitAll()
                         .anyRequest().authenticated()
+                )
+
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
+
         return http.build();
     }
 
